ef33e8a16bf6599632d69928cad5edd98a1cfa74
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-01-19 12:58:24 -0800

    added user model

--- oh_connection/admin.py
+++ oh_connection/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import OpenHumansMember
+
 # Register your models here.
+
+@admin.register(OpenHumansMember)
+class OpenHumansMemberAdmin(admin.ModelAdmin):
+    pass
--- oh_data_uploader/settings.py
+++ oh_data_uploader/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oh_connection.apps.OhConnectionConfig'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'oh_data_uploader.urls'
@@ -117,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
