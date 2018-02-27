a1c9e4127dd36019e1b7227d379d763d15f61b35
Jasmine <36580695+jasmine1997@users.noreply.github.com>
2018-02-28 02:20:45 +0530

    Add list and delete functionality (#14)
    * Add list and delete functionality
    
    * Fix conflicts

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^logout/?$', views.logout_user, name='logout'),
     url(r'^overview/?$', views.overview, name='overview'),
     url(r'^upload_simple/?$', views.upload_old, name='upload_old'),
-    url(r'^about/?$', views.about, name='about')
+    url(r'^about/?$', views.about, name='about'),
+    url(r'^list/?$', views.list_files, name='list'),
+    url(r'^delete/(?P<file_id>\w+)/?$', views.delete_file, name='delete')
 ]
