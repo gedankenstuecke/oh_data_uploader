033944eb6729350872858352019e1d4c3d66e19b
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-02-27 15:13:46 -0800

    first test! :D

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+from django.core.management import call_command
+from django.conf import settings
+from open_humans.models import OpenHumansMember
 
-# Create your tests here.
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        settings.DEBUG = True
+        call_command('init_proj_config')
+        self.factory = RequestFactory()
+        self.oh_member = OpenHumansMember.create(oh_id='1234567890abcdef',
+                                                 access_token='foo',
+                                                 refresh_token='bar',
+                                                 expires_in=2000)
+        self.oh_member.save()
+        self.user = self.oh_member.user
+        self.user.set_password('foobar')
+        self.user.save()
+
+    def test_index_logged_out(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_logged_in(self):
+        c = Client()
+        c.login(username=self.user.username, password='foobar')
+        response = c.get("/")
+        print(response.content)
+        self.assertRedirects(response, '/overview')
