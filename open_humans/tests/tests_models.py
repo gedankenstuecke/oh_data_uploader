cca993c9036e0722fd7ee6ddce96ee4887b16ac3
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-03-01 10:33:19 -0800

    test the hound (#35)
    * test the hound
    
    * just as example.
    
    * change setup?
    
    * test again
    
    * satisfy the 🐶

--- open_humans/tests/tests_models.py
+++ open_humans/tests/tests_models.py
@@ -3,12 +3,15 @@ from django.core.management import call_command
 from django.conf import settings
 from open_humans.models import OpenHumansMember
 
+
 class OpenHumansMemberTest(TestCase):
     def setUp(self):
         settings.DEBUG = True
         call_command('init_proj_config')
-        self.oh_member=OpenHumansMember(oh_id=1234, access_token='foo', refresh_token='bar')
+        self.oh_member = OpenHumansMember(oh_id=1234,
+                                          access_token='foo',
+                                          refresh_token='bar')
 
-    def tests_str_(self): 
-        self.assertEqual(str(self.oh_member), "<OpenHumansMember(oh_id='1234')>")
-           
+    def tests_str_(self):
+        self.assertEqual(str(self.oh_member),
+                         "<OpenHumansMember(oh_id='1234')>")
