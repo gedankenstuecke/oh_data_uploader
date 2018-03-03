fc4720517ca2693a1fa84c37ea55297b0832a012
Manaswini Das <dasmanaswini10@gmail.com>
2018-03-04 00:25:52 +0530

    Removes style violations

--- project_admin/tests/tests_views.py
+++ project_admin/tests/tests_views.py
@@ -3,23 +3,23 @@ from django.core.management import call_command
 from django.conf import settings
 
 
-class User_Views_Test( TestCase ):
+class User_Views_Test(TestCase):
 
-    def setUp( self ):
+    def setUp(self):
         """
         Set up the app for following tests.
         """
         settings.DEBUG = True
         settings.ADMIN_PASSWORD = '123'
-        call_command( 'init_proj_config' )
+        call_command('init_proj_config')
 
     def test_project_config_view(self):
-        response = self.client.get( "/project-admin/" )
-        self.assertRedirects( response, '/project-admin/login' )
-        self.assertEqual( response.status_code, 302 )
+        response = self.client.get("/project-admin/")
+        self.assertRedirects(response, '/project-admin/login')
+        self.assertEqual(response.status_code, 302)
 
     def test_project_config_page_returns_correct_html(self):
         c = Client()
-        response = c.post( "/project-admin/login", {'user': 'admin', 'password': '123'}, follow=True )
-        self.assertContains( response, '<h1>Current Project Configuration</h1>', status_code=200 )
-        self.assertTemplateUsed( response, 'project_admin/home.html' )
+        response = c.post("/project-admin/login", {'user': 'admin', 'password': '123'}, follow=True)
+        self.assertContains(response, '<h1>Current Project Configuration</h1>', status_code=200)
+        self.assertTemplateUsed(response, 'project_admin/home.html')
