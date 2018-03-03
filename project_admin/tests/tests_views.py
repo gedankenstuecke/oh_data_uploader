f5274685a9106a9ad7eaa15b0efe7a86281b78a6
Manaswini Das <dasmanaswini10@gmail.com>
2018-03-04 00:16:04 +0530

    Removes style violations

--- project_admin/tests/tests_views.py
+++ project_admin/tests/tests_views.py
@@ -15,14 +15,15 @@ class User_Views_Test(TestCase):
         call_command('init_proj_config')
 
     def test_project_config_view(self):
-        user_login = self.client.login(password="123")
         response = self.client.get("/project-admin/")
         self.assertRedirects(response, '/project-admin/login')
         self.assertEqual(response.status_code, 302)
 
     def test_project_config_page_returns_correct_html(self):
-        request = HttpRequest()
-        c=Client()
-        response = c.post("/project-admin/login",{'user':'admin','password':'123'},follow=True)
-        self.assertContains( response, '<h1>Current Project Configuration</h1>', status_code=200, html=False )
-        self.assertTemplateUsed( response, 'project_admin/home.html' )
+        c = Client()
+        response = c.post( "/project-admin/login",{'user': 'admin', 
+                                                   'password': '123'}, 
+                                                    follow=True ) 
+        self.assertContains( response, '<h1>Current Project Configuration</h1>', 
+                                                    status_code=200 ) 
+        self.assertTemplateUsed( response, 'project_admin/home.html' ) 
