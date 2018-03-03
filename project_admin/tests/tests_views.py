a7e09995e17a26b497975975e20cdaa618b98a89
Manaswini Das <dasmanaswini10@gmail.com>
2018-03-04 00:02:34 +0530

    Adds tests for Project-Config

--- project_admin/tests/tests_views.py
+++ project_admin/tests/tests_views.py
@@ -2,11 +2,9 @@ from django.test import TestCase, Client
 from django.http import HttpRequest
 from django.core.management import call_command
 from django.conf import settings
-from django.template import RequestContext
-from django.template.loader import render_to_string
 
 
-class ProjectConfig_Views_Test(TestCase):
+class User_Views_Test(TestCase):
 
     def setUp(self):
         """
@@ -16,15 +14,15 @@ class ProjectConfig_Views_Test(TestCase):
         settings.ADMIN_PASSWORD = '123'
         call_command('init_proj_config')
 
-    def test_home_view(self):
+    def test_project_config_view(self):
         user_login = self.client.login(password="123")
         response = self.client.get("/project-admin/")
+        self.assertRedirects(response, '/project-admin/login')
         self.assertEqual(response.status_code, 302)
 
-    def test_home_page_returns_correct_html(self):
+    def test_project_config_page_returns_correct_html(self):
         request = HttpRequest()
         c=Client()
-        response = c.post("/project-admin/",{'user':'admin','password':'123'},follow=True)
-        self.assertContains( response, '<h1>Current Project Configuration</h1>', status_code=200, html=True )
+        response = c.post("/project-admin/login",{'user':'admin','password':'123'},follow=True)
+        self.assertContains( response, '<h1>Current Project Configuration</h1>', status_code=200, html=False )
         self.assertTemplateUsed( response, 'project_admin/home.html' )
-        
