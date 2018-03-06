fd14be4d810c1f798dcfef57648ea9792a41b957
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-03-06 13:45:25 -0800

    add tests for upload functions (#60)

--- main/tests/tests_views.py
+++ main/tests/tests_views.py
@@ -1,10 +1,12 @@
 from django.test import TestCase, Client
 from django.core.management import call_command
 from django.conf import settings
-from project_admin.models import ProjectConfiguration
+from project_admin.models import ProjectConfiguration, FileMetaData
 from open_humans.models import OpenHumansMember
 import markdown
 import vcr
+import json
+from io import BytesIO
 
 
 class AboutPageTestCase(TestCase):
@@ -111,3 +113,56 @@ class LoginTestCase(TestCase):
         response = c.get("/delete/1337", follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/list.html')
+
+
+class UploadTestCase(TestCase):
+    def setUp(self):
+        # set up project
+        settings.DEBUG = True
+        # settings.OPENHUMANS_APP_BASE_URL = "http://127.0.0.1"
+        call_command('init_proj_config')
+        project_config = ProjectConfiguration.objects.get(id=1)
+        project_config.oh_client_id = "6yNYmUlXN1wLwQFQR0lnUohR1KMeVt"
+        project_config.oh_client_secret = "Y2xpZW50aWQ6Y2xpZW50c2VjcmV0"
+        project_config.save()
+
+        self.oh_file = FileMetaData()
+        self.oh_file.name = "test"
+        self.oh_file.description = 'a test file'
+        self.oh_file.tags = json.dumps("my,tag,list")
+        self.oh_file.save()
+
+        # set up user
+        data = {"access_token": 'foo',
+                "refresh_token": 'bar',
+                "expires_in": 36000}
+        self.oh_member = OpenHumansMember.create(oh_id='1234567890abcdef',
+                                                 data=data)
+        self.oh_member.save()
+        self.user = self.oh_member.user
+        self.user.set_password('foobar')
+        self.user.save()
+
+    def test_get_upload_old(self):
+        c = Client()
+        c.login(username=self.user.username, password='foobar')
+        response = c.get("/upload_simple/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "a test file")
+        self.assertTemplateUsed(response, 'main/upload_old.html')
+
+    @vcr.use_cassette('main/tests/fixtures/upload_file.yaml',
+                      record_mode='none')
+    def test_post_upload_old(self):
+        c = Client()
+        c.login(username=self.user.username, password='foobar')
+        test_file = BytesIO(b'mybinarydata')
+        test_file.name = 'myimage.jpg'
+        response = c.post("/complete/",
+                          {'file_1': test_file},
+                          follow=True)
+        self.assertRedirects(response,
+                             "/overview",
+                             status_code=302)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/overview.html')
